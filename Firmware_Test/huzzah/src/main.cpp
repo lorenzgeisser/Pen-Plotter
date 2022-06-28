3867a713318013f7fea5942e32468f9d8628e559
Lorenz Geisser <geisserlorenz@gmail.com>
2022-06-28 21:44:03 +0200

    other lib

--- Firmware_Test/huzzah/src/main.cpp
+++ Firmware_Test/huzzah/src/main.cpp
@@ -1,82 +1,53 @@
 #include <Arduino.h>
+#include <TMC2209.h>
 
-/**
- * Author Teemu Mäntykallio
- *
- * Plot TMC2130 or TMC2660 motor load using the stallGuard value.
- * You can finetune the reading by changing the STALL_VALUE.
- * This will let you control at which load the value will read 0
- * and the stall flag will be triggered. This will also set pin DIAG1 high.
- * A higher STALL_VALUE will make the reading less sensitive and
- * a lower STALL_VALUE will make it more sensitive.
- *
- * You can control the rotation speed with
- * 0 Stop
- * 1 Resume
- * + Speed up
- * - Slow down
- */
-#include <TMCStepper.h>
+#define EN_PIN 14   // Enable
+#define DIR_PIN 15  // Direction
+#define STEP_PIN 32 // Step
 
-#define MAX_SPEED        40 // In timer value
-#define MIN_SPEED      1000
+HardwareSerial &serial_stream = Serial1;
 
-#define STALL_VALUE     100 // [0..255]
+const long SERIAL_BAUD_RATE = 115200;
+const int DELAY = 3000;
 
-#define EN_PIN           14 // Enable
-#define DIR_PIN          15 // Direction
-#define STEP_PIN         32 // Step
-#define SERIAL_PORT Serial1 // TMC2208/TMC2224 HardwareSerial port
-#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
+// Instantiate TMC2209
+TMC2209 stepper_driver;
 
-#define R_SENSE 0.11f // Match to your driver
-                      // SilentStepStick series use 0.11
-                      // UltiMachine Einsy and Archim2 boards use 0.2
-                      // Panucatt BSD2660 uses 0.1
-                      // Watterott TMC5160 uses 0.075
-
-// Select your stepper driver type
-TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
-//TMC2209Stepper driver(SW_RX, SW_TX, R_SENSE, DRIVER_ADDRESS);
-
-using namespace TMC2208_n;
-
-// Using direct register manipulation can reach faster stepping times
-#define STEP_PORT     PORTF // Match with STEP_PIN
-#define STEP_BIT_POS      0 // Match with STEP_PIN
-
-
-
-void setup() {
-  Serial.begin(115200);         // Init serial port and set baudrate
-  while(!Serial);               // Wait for serial port to connect
-  Serial.println("\nStart...");
-
-  SERIAL_PORT.begin(115200);
-
-  pinMode(EN_PIN, OUTPUT);
-  pinMode(STEP_PIN, OUTPUT);
-  pinMode(DIR_PIN, OUTPUT);
+void setup()
+{
+    delay(4000);
+    pinMode(EN_PIN, OUTPUT);
+    pinMode(STEP_PIN, OUTPUT);
+    pinMode(DIR_PIN, OUTPUT);
     digitalWrite(DIR_PIN, HIGH);
+    digitalWrite(EN_PIN, LOW);
 
-  digitalWrite(EN_PIN, LOW);
+    Serial.begin(SERIAL_BAUD_RATE);
 
-  driver.begin();
-  driver.rms_current(400); // mA
-  driver.microsteps(8);
-  driver.TCOOLTHRS(0xFFF0F); // 20bit max
-  driver.semin(5);
-  driver.semax(2);
-  driver.sedn(0b01);
-  driver.SGTHRS(STALL_VALUE);
+    stepper_driver.setup(serial_stream);
 
-   Serial.println(driver.version(),HEX);
+    if (stepper_driver.isSetupAndCommunicating())
+    {
+        Serial.println("Stepper driver is setup and communicating!");
+        Serial.println("Try turning driver power off to see what happens.");
+    }
+    else if (stepper_driver.isCommunicatingButNotSetup())
+    {
+        Serial.println("Stepper driver is communicating but not setup!");
+        Serial.println("Running setup again...");
+        stepper_driver.setup(serial_stream);
+    }
+    else
+    {
+        Serial.println("Stepper driver is not communicating!");
+        Serial.println("Try turning driver power on to see what happens.");
+    }
 
+    stepper_driver.enable();
+
+    stepper_driver.setHoldCurrent(100);
 }
 
-void loop() {
-  digitalWrite(STEP_PIN,HIGH);
-  delay(1);
-  digitalWrite(STEP_PIN,LOW);
-  delay(1);
+void loop()
+{
 }
